a87c1a72d25c915886e703afa3d86faf98c7ab20
Connor Milliken <cmilliken09@gmail.com>
2019-08-20 14:58:12 -0400

    Added Chat

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+from app import app, socketio
+from flask import render_template, redirect, url_for
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
